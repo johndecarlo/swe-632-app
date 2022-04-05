c8714315b048d3d3142d1db1af1117c23f5828b3
John DeCarlo <jdecarlo841@gmail.com>
2022-04-05 15:05:14 -0400

    Find and Sort added
    Added a search bar and a filter to the different books and movies that we have implemented within our search feature

--- src/routes/category/index.js
+++ src/routes/category/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Select from 'react-select';
 import Item from '../../components/item';
 import data from '../../data/data.json';
 import { useParams } from 'react-router-dom';
@@ -9,7 +10,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Category = () => {
   const [categoryData, setData] = useState([]);
   const [categoryTitle, setTitle] = useState('');
+  const [query, setQuery] = useState("");
   const { type } = useParams();
+
+  const options = [
+    { value: 'recent', label: 'Most Recent' },
+    { value: 'alphabet', label: 'A-Z' },
+    { value: 'ratingHigh', label: 'Highest Rating' },
+    { value: 'ratingLow', label: 'Lowest Rating' }
+  ]
 
   useEffect(() => {
     setTitle(type.charAt(0).toUpperCase() + type.slice(1));
@@ -21,6 +30,24 @@ const Category = () => {
     }
   }, [type]);
 
+  categoryData.filter(post => {
+      if (query === '') {
+        return post;
+      } else if (post.title.toLowerCase().includes(query.toLowerCase())) {
+        return post;
+      }
+    }).map((item, i) => {
+      const title = item.title;
+      return (
+        <Item
+          key={i}
+          title={title}
+          countLikes={item.countLikes}
+          countDislikes={item.countDislikes}
+        />
+      );
+    })
+
   return (
     <div className="category-component">
       <div className="category-header">
@@ -28,6 +55,16 @@ const Category = () => {
           <FontAwesomeIcon icon={faArrowCircleLeft} />
         </Link>
         <h1 className="category-title">{categoryTitle}</h1>
+      </div>
+      <div class="flex-container">
+        <div class="flex-child magenta">
+          <h4>Find {categoryTitle}:</h4>
+          <input placeholder=" Search" onChange={event => setQuery(event.target.value)} />
+        </div>
+        <div class="flex-child green">
+          <h4>Sort {categoryTitle}:</h4>
+          <Select options={options} />
+        </div>
       </div>
       <div className="categories">
         <div>
