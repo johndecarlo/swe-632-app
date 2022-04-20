487c31d85df000beabb884a679f98310303eba80
Alan <alan@Alans-MacBook-Pro.local>
2022-04-20 00:20:41 -0400

    other changes

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,65 +1,63 @@
 import { NavLink } from 'react-router-dom';
-import main from '../../app/assets/main.jpg';
 import {ReactComponent as BookIcon} from '../../app/assets/book-line.svg';
 import {ReactComponent as MovieIcon} from '../../app/assets/movie.svg';
 import {ReactComponent as RestaurantIcon} from '../../app/assets/food-restaurant.svg';
 import {ReactComponent as TVIcon} from '../../app/assets/screen.svg';
 
-const Home = () => (
-	<div className="component">
+const Home = () => {
+	return(
+		<div className="component">
 		<div className="category-container">
-			<div className="category">
-				<div>
-					<BookIcon className="category-icon" />
+			<NavLink to="/category/books">
+				<div className="category">
+					<div>
+						<BookIcon className="category-icon" />
+					</div>
+					<div>
+						<button className="category-title">Books</button>
+					</div>
 				</div>
-				<div>
-					<NavLink to="/category/books">
-						Books
-					</NavLink>
-				</div>
-			</div>
+			</NavLink>	
 
-			<div className="category">
-				<div>
-					<MovieIcon className="category-icon" />
+			<NavLink to="/category/movies">
+				<div className="category">
+					<div>
+						<MovieIcon className="category-icon" />
+					</div>
+					<div>
+						<button className="category-title">Movies</button>
+					</div>
 				</div>
-				<div>
-					<NavLink to="/category/movies">
-						Movies
-					</NavLink>
-				</div>
-			</div>
+			</NavLink>
 
-			<div className="category">
-				<div>
-					<RestaurantIcon className="category-icon" />
+			<NavLink to="/category/restaurants">
+				<div className="category">
+					<div>
+						<RestaurantIcon className="category-icon" />
+					</div>
+					<div>
+						<button className="category-title">Restaurant</button>
+					</div>
 				</div>
-				<div>
-					<NavLink to="/category/restaurants">
-						Restaurants
-					</NavLink>
-				</div>
-			</div>
+			</NavLink>
 
-			<div className="category">
-				<div>
-					<TVIcon className="category-icon" />
+			<NavLink to="/category/television">
+				<div className="category">
+					<div>
+						<TVIcon className="category-icon" />
+					</div>
+					<div>
+						<button className="category-title">Television</button>
+					</div>
 				</div>
-				<div>
-					<NavLink to="/category/television">
-						Television
-					</NavLink>
-				</div>
-			</div>
+			</NavLink>	
+
 		</div>
 		<div className="search-container">
-			<div className="search-bar-container">
-				{/* <h1 className="main-search">Reviews</h1> */}
-				{/* <Search className="main-search"/> */}
-			</div>
-			<img src={main} alt="main" className="main-image"/>
+
 		</div>
 	</div>
-);
+	)
+}
 
 export default Home;
