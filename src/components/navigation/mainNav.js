2a6166beaf8c3433354a02bcfa79238a83b98b40
Alan <alan@Alans-MacBook-Pro.local>
2022-04-27 14:37:55 -0400

    removing unneeded code

--- src/components/navigation/mainNav.js
+++ src/components/navigation/mainNav.js
@@ -5,24 +5,8 @@ const MainNav = () => {
   return (
     <div className="navigation-links">
       <div className="navigation-link-container">
-        {/* {(<NavLink
-          to="/signin"
-          children = "Sign in"
-          className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
-        />
-        )} */}
-
         <Link className="home-link" to="/">Home</Link>
       </div>
-
-
-      {/* {(<NavLink
-        to="/profile"
-        children = "Profile"
-        className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
-      />
-      )}  */}
-
     </div>
   );
 }
